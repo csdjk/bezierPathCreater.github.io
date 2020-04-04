61fb28a66354e1a551cf148207a2464049b682de
csdjk <1076984375@qq.com>
2020-04-04 17:46:38 +0800

    update resolution

--- main.a2ad0.js
+++ main.a2ad0.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.3382d.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,12 +178,12 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.js');
+        require('src/settings.717c5.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.js');
+        require('src/settings.717c5.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
